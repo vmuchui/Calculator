0556b91ecf5a8b19249b3d1b5694aa0e3757b2ec
vmuchui <kaigongimuchui@gmail.com>
2019-03-29 12:32:00 +0300

    this commit adds card style to the file along with the multiplication function

--- js/scripts.js
+++ js/scripts.js
@@ -12,15 +12,25 @@ var multiply = function(number1, number2) {
 var divide = function(number1, number2) {
  return number1 / number2;
 };
+var modulo = function(number1, number2) {
+  return number1 % number2;
+}
 
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
 $("form#add").submit(function(event) {
 event.preventDefault();
-var number1 = parseInt($("#add1").val());
-var number2 = parseInt($("#add2").val());
+var number1 = parseFloat($("#add1").val());
+var number2 = parseFloat($("#add2").val());
 var result = add(number1, number2);
 $("#output").text(result);
+});
+$("form#multiply").submit(function(event) {
+event.preventDefault();
+var number1 = parseFloat($("#multiply1").val());
+var number2 = parseFloat($("#multiply2").val());
+var result = multiply(number1, number2);
+$("#output2").text(result);
 });
 });
