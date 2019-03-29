b0ed2c884f16fb48bcfb61b8fd0ae72facadfc56
vmuchui <kaigongimuchui@gmail.com>
2019-03-29 11:37:10 +0300

    this commmit adds an addition calculator with a fully functioning user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,26 @@
-var add = function(number1,number2){
-return number1 + number2;
+// Business (or back-end) logic:
+
+var add = function(number1, number2) {
+ return number1 + number2;
 };
-var number1 = prompt("Enter a number!");
-alert(number1);
-var add = function(number1,number2){
-return number1 + number2;
+var subtract = function(number1, number2) {
+ return number1 - number2;
 };
-var number1 = prompt("Enter a number!");
-var number2 = prompt("Enter another number!");
-alert(add(number1,number2));
-var number1 = parseInt(prompt("Enter a number!"));
-var number2 = parseInt(prompt("Enter another number!"));
-alert(add(number1,number2));
+var multiply = function(number1, number2) {
+ return number1 * number2;
+};
+var divide = function(number1, number2) {
+ return number1 / number2;
+};
+
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function() {
+$("form#add").submit(function(event) {
+event.preventDefault();
+var number1 = parseInt($("#add1").val());
+var number2 = parseInt($("#add2").val());
+var result = add(number1, number2);
+$("#output").text(result);
+});
+});
